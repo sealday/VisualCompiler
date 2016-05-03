57417b988b1fb55e934366d02030ff6c246dae16
xinglianfly <2650454635@qq.com>
2016-05-03 22:57:15 +0800

    first集合

--- js/FirstFollowSet.js
+++ js/FirstFollowSet.js
@@ -12,63 +12,15 @@ class FirstSet{
      */
     _findFirst(raw_grammar) {
 
-
-
-    // var userinput = ["S->ABc","A->a|$","B->b"];
-    var userinput = ["E->TE'",
-        "E'->+TE'|ε",
-        "T->FT'",
-        "T'->*FT'|ε",
-        "F->(E)|i"
-    ];
-
-    // var userinput = ["A->BaAb",
-    //    "B->l",
-    //     "C->eA",
-    // ];
-
-
-        // raw_grammar.forEach((current)=>{
-        //    map[current.head] = current.body;
-        // });
-    for (var i = 0; i < userinput.length; i++) {
-        var split1 = userinput[i].split("->");
-        var split2 = split1[1].split("|");
-        this.map[split1[0]] = split2;
-    }
-    Object.size = function (map) {
-        var size = 0, key;
-        for (key in map) {
-            if (map.hasOwnProperty(key))
-                size++;
-        }
-        return size;
-    };
-
-        // raw_grammar.forEach((current)=>{
-        //     this.map[current.head] = current.body;
-        //    this._firstCore(current.head,current.body);
-        // });
-    // for (var b = 0; b < Object.size(this.map); b++) {
-    //     this._firstCore(Object.keys(this.map)[b], this.map[Object.keys(this.map)[b]]);
-    // }
-
+        raw_grammar.forEach((current)=>{
+            this.map[current.head] = current.body;
+        });
+        raw_grammar.forEach((current)=>{
+           this._firstCore(current.head,current.body);
+        });
 
     console.log("first 集合如下:" + this.first);
 
-    // var keyset = Object.keys(this.first);
-    // for (var kn = 0; kn < keyset.length; kn++) {
-    //     var valueset = this.first[keyset[kn]];
-    //     home.firstinput += keyset[kn] + ":{";
-    //     for (var vn = 0; vn < valueset.length; vn++) {
-    //         home.firstinput += valueset[vn] + ",";
-    //     }
-    //     home.firstinput += "}\n";
-    // }
-    //
-    // home.firstinput += "follow集合:\n";
-
-    // findFollow(userinput, first);
 
     
 }
@@ -78,42 +30,12 @@ class FirstSet{
         return this.first[leftnode];
     }
 
-    // var st = [];
-    //     for (var ii = 0; ii < rightnodes.length; ii++) {
-    //         for (var aa = 0; aa < rightnodes[ii].length; aa++) {
-    //             let currentbody = rightnodes[ii][aa];
-    //             if(currentbody in this.map){//非终结点
-    //                 var temst = this._firstCore(currentbody, this.map[currentbody]);
-    //                 st.concat(temst);
-    //                 if (temst.indexOf("$") > -1) {
-    //
-    //                 } else {
-    //                     break;
-    //
-    //                 }
-    //             }else{//终结点
-    //                 st.push(currentbody);
-    //
-    //             }
-    //
-    //         }
-    //     }
-    //
-    //     this.first[leftnode] = st;
-    //     return st;
-
-    for (var ii = 0; ii < rightnodes.length; ii++) {
-        for (var aa = 0; aa < rightnodes[ii].length; aa++) {
-            var nextnode = rightnodes[ii].charAt(aa);
-            if (nextnode in this.map) {//非终结点
-    
-                if (aa + 1 < rightnodes[ii].length && rightnodes[ii].charAt(aa + 1) == '\'') {
-                    nextnode += rightnodes[ii].charAt(aa + 1);
-                    ++aa;
-                }
-
-                if (nextnode in this.map) {
-                    var temst = this._firstCore(nextnode, this.map[nextnode]);
+    var st = [];
+        for (var ii = 0; ii < rightnodes.length; ii++) {
+            for (var aa = 0; aa < rightnodes[ii].length; aa++) {
+                let currentbody = rightnodes[ii][aa];
+                if(currentbody in this.map){//非终结点
+                    var temst = this._firstCore(currentbody, this.map[currentbody]);
                     st = st.concat(temst);
                     if (temst.indexOf("$") > -1) {
 
@@ -121,17 +43,16 @@ class FirstSet{
                         break;
 
                     }
+                }else{//终结点
+                    st.push(currentbody);
+                    break;
+
                 }
 
-
-            } else {//终结点
-                st.push(nextnode.charAt(0));
-                break;
             }
         }
-    }
 
-    first[leftnode] = st;
-    return st;
+        this.first[leftnode] = st;
+        return st;
 }
 }
